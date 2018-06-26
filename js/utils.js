fceb12d697d4f64da9af703f8c44576dd5162081
vinyll <vincent.agnano@scopyleft.fr>
2018-06-26 14:17:05 +0200

    linted js/utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,16 +1,16 @@
-function distance(latlng1, latlng2) {
+function distance (latlng1, latlng2) {
   const [lat1, lng1] = latlng1
   const [lat2, lng2] = latlng2
   const R = 6371 // Radius of the earth in km
-  const dLat = deg2rad(lat2 - lat1)  // deg2rad below
+  const dLat = deg2rad(lat2 - lat1) // deg2rad below
   const dLon = deg2rad(lng2 - lng1)
-  const a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+  const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
             Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
-            Math.sin(dLon/2) * Math.sin(dLon/2)
-  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
+            Math.sin(dLon / 2) * Math.sin(dLon / 2)
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
   return R * c // Distance in km
 }
 
-function deg2rad(deg) {
+function deg2rad (deg) {
   return deg * (Math.PI / 180)
 }
