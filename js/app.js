ad575e6c1ffa21905e81def515702790ea8af6b4
rap2h <raphaelht@gmail.com>
2018-07-13 12:16:23 +0200

    es6 transpilation

--- js/app.js
+++ js/app.js
@@ -1,67 +1,31 @@
-const apiUrl = localStorage.getItem('apiUrl') || 'https://eac-api.herokuapp.com'
+'use strict';
 
-const includeJs = require([
-  '/vendors/js/riot+compiler.min.js',
-  '/vendors/js/page.js',
-  '/vendors/js/leaflet.js',
-  '/vendors/js/awesomplete.min.js',
-])
+var apiUrl = localStorage.getItem('apiUrl') || 'https://eac-api.herokuapp.com';
 
-requireTags([
-  '/tags/app.tag.html',
-  '/tags/pages/search.tag.html',
-  '/tags/pages/actor.tag.html',
-  '/tags/pages/actor-edit.tag.html',
-  '/tags/partials/search-filters.tag.html',
-  '/tags/partials/actor-card.tag.html',
-  '/tags/partials/actors-map.tag.html',
-  '/tags/partials/color-tag.tag.html',
-  '/tags/partials/search-form.tag.html',
-  '/tags/partials/select-school.tag.html',
-])
+function request(url) {
+  var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
 
-
-includeJs.then(() => riot.mount('*'))
-
-function request (url, options = {}) {
-  const params = Object.assign(options, {
-    mode: 'cors',
-  })
-  return fetch(url, params)
-    .then(r => r.json())
-    .catch(console.error.bind(console))
+  var params = Object.assign(options, {
+    mode: 'cors'
+  });
+  return fetch(url, params).then(function (r) {
+    return r.json();
+  }).catch(console.error.bind(console));
 }
 
-function api (uri, options) {
-  return request(`${apiUrl}${uri}`, options)
+function api(uri, options) {
+  return request('' + apiUrl + uri, options);
 }
 
-function urlParams(queryString = location.search) {
-  const keyValues = queryString.startsWith('&') ? queryString.slice(1) : queryString
-  return new Map(keyValues.split('&').map(kv => kv.split('=')))
+function urlParams() {
+  var queryString = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : location.search;
+
+  var keyValues = queryString.startsWith('&') ? queryString.slice(1) : queryString;
+  return new Map(keyValues.split('&').map(function (kv) {
+    return kv.split('=');
+  }));
 }
 
-function normalizeString (str) {
-  return str.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '')
-}
-
-function require(sources, type = 'text/javascript') {
-  return new Promise((resolve) => {
-    let loadedCount = 0
-    sources.forEach(src => {
-      const script = document.createElement('script')
-      script.onload = () => {
-        loadedCount++
-        if(loadedCount === sources.length) {
-          resolve()
-        }
-      }
-      script.src = src
-      script.type = type
-      document.head.appendChild(script)
-    })
-  })
-}
-function requireTags(sources) {
-  return require(sources, 'riot/tag')
+function normalizeString(str) {
+  return str.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '');
 }
