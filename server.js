ccdb2581d788e5510636573ae166e44375fb543b
rap2h <raphaelht@gmail.com>
2018-09-10 11:04:02 +0200

    quick fix

--- server.js
+++ server.js
@@ -4,14 +4,6 @@ const serveStatic = require('serve-static')
 const app = express()
 const port = process.env.PORT || 8080
 
-
-app.use((req, res, next) => {
-  if (!req.secure) {
-    return res.redirect(`https://${req.get('Host')}${req.url}`)
-  }
-  next()
-})
-
 app.use(serveStatic(__dirname))
 
 app.all('*', (_req, res) => res.sendFile(`${__dirname}/index.html`))
