8e78d78f1397bc1edd71cc14a4141d6724b5f62e
vinyll <vincent.agnano@scopyleft.fr>
2018-09-10 10:26:42 +0200

    enforced https

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ const serveStatic = require('serve-static')
 const app = express()
 const port = process.env.PORT || 8080
 
+
+app.use((req, res, next) => {
+  if (!req.secure) {
+    return res.redirect(`https://${req.get('Host')}${req.url}`)
+  }
+  next()
+})
+
 app.use(serveStatic(__dirname))
+
 app.all('*', (_req, res) => res.sendFile(`${__dirname}/index.html`))
 app.listen(port, () => console.log(`Listening on port ${port}…`))
